20982e490d02e77973ec41855c6c158e69fd5319
Joel-IC <joel.ic2000@hotmail.com>
2025-03-24 22:02:19 -0500

    ➕ ADD: Implement crearMarca and listarMarcas methods in MarcaController; update routes for marca creation and listing

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    
+    public function crearMarca(Request $request){
+        //Instanciamos la clase
+        $marca = new Marca();
+
+        //Asignamos los valores
+        $request->validate([
+            'marca_nombre' => 'required|string|max:100',
+            'marca_origen' => 'required|string|max:100'
+        ]);
+        
+        $marca->marca_nombre = $request->input('marca_nombre');
+        $marca->marca_origen = $request->input('marca_origen');
+
+        //Guardamos
+        $marca->save();
+
+        //Respuesta
+        return response()->json([
+            'status' => true,
+            'marca' => $marca
+        ],201);
+    }
+
+    public function listarMarcas(){
+
+        // llamar a todas las marcas
+        $marcas = Marca::all();
+
+        // Respuesta
+        return response()->json([
+            'status' => true,
+            'marcas' => $marcas
+        ],200);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\MarcaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('/crear-marca', [MarcaController::class, 'crearMarca']);
